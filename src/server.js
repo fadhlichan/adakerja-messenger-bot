cc6b7bc83037e70ba52268ac084ce17b7b6084a1
fadhlichan <fadhlisyaifullah@gmail.com>
2022-01-17 21:26:27 +0700

    Update: server start function

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 require('./utils/dotenv')
 const mongoose = require('mongoose')
 const app = require('./app')
-const PORT = process.env.PORT || 1337
+const db = require('./db')
 
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
+const PORT = process.env.PORT || 1337
+const MONGO_URI = process.env.MONGO_URI
+
+const main = async () => {
+    try {
+        await db.connect(MONGO_URI)
         app.listen(PORT, () => {
             console.log('Webhook listening on port', PORT)
         })
-    }).catch(e => {
-        console.error(E)
-    })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+main()
